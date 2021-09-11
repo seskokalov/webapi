7a3f036bde4bbbe1f25894928a93a5117f5ae197
seskokalov <78028357+seskokalov@users.noreply.github.com>
2021-09-11 19:30:24 +0200

    first homework added

---
+++ Class02.Homework/Class02.Homework/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Class02.Homework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return StatusCode(StatusCodes.Status200OK, StaticDB.Users);
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            try
+            {
+                if (id < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "ID cannot be < 0.");
+                }
+                if (id >= StaticDB.Users.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, "ID out of range");
+                }
+                return StatusCode(200, StaticDB.Users[id]);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server Error");
+            }
+        }
+
+        [HttpGet("route")]
+        public ActionResult<string> GetPath()
+        {
+            var request = Request;
+            return StatusCode(StatusCodes.Status200OK, request.Path.ToString());
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
