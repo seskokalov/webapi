87282109c716b89b82d1829b39941d3176f7465c
seskokalov <78028357+seskokalov@users.noreply.github.com>
2021-09-15 19:29:02 +0200

    workshop app added

---
+++ Movie.Workshop.V1/Movie.Workshop.V1/Controllers/MoviesController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Workshop.V1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Movies.Workshop.V1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            try
+            {
+                return StatusCode(StatusCodes.Status200OK, StaticDB.Movies);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Movie> Get(int id)
+        {
+            try
+            {
+                if (id < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "Bad request!");
+                }
+
+                if (id >= StaticDB.Movies.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, "Movie does not exist!");
+                }
+
+                return StatusCode(StatusCodes.Status200OK, StaticDB.Movies[id]);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("searchbygenre")]
+        public ActionResult<Movie> Get(string genre)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(genre))
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, "Enter a genre!");
+                }
+
+                Movie theMovie = StaticDB.Movies.FirstOrDefault(x => x.Genre.ToLower().Contains(genre.ToLower()));
+                return StatusCode(StatusCodes.Status200OK, theMovie.Title.ToString());
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server Error");
+            }
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost("addmovie")]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            try
+            {
+                StaticDB.Movies.Add(movie);
+                return StatusCode(StatusCodes.Status201Created, "Movie added!");
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error!");
+            }
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{idMovie}")]
+        public IActionResult Put(int idMovie, [FromBody] string description)
+        {
+            try
+            {
+                if (idMovie < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "Bad request!");
+                }
+
+                if (idMovie >= StaticDB.Movies.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, "Movie does not exist!");
+                }
+
+                Movie movie = StaticDB.Movies[idMovie];
+                if (movie.Description == null)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "Please enter a description!");
+                }
+
+                movie.Description = description;
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{index}")]
+        public IActionResult Delete(int index)
+        {
+            try
+            {
+                StaticDB.Movies.RemoveAt(index);
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server Error");
+            }
+        }
+    }
+}
---
+++ Movie.Workshop.V1/Movie.Workshop.V1/StaticDB.cs
@@ -0,0 +1,36 @@
+﻿using Movies.Workshop.V1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Workshop.V1
+{
+    public static class StaticDB
+    {
+        public static List<Models.Movie> Movies = new List<Models.Movie>()
+        {
+            new Models.Movie()
+            {
+                Title = "The Shawshank Redemption",
+                Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+                Year = 1994,
+                Genre = "Drama"
+            },
+            new Models.Movie()
+            {
+                Title = "The Godfather",
+                Description = "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.",
+                Year = 1972,
+                Genre = "Drama"
+            },
+            new Models.Movie()
+            {
+                Title = "The Dark Knight",
+                Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+                Year = 2008,
+                Genre = "Action"
+            }
+        };
+    }
+}
